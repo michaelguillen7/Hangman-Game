1c31fd166b4c779c1b169511e7c9586e38d2ab49
Michael Guillen <michaelguillen@Michaels-MacBook-Pro.local>
2018-07-26 19:50:38 -0500

    added start game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,3 +62,37 @@ function reset()
 	test=false;
 	startGame();
 }
+
+function startGame()
+{
+	choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	lettersInWord = choosenWord.split('');
+	numBlanks = lettersInWord.length;
+	
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	doubleWord = ['a','b','c',
+					  'd','e','f',
+					  'g','h','i',
+					  'j','k','l',
+					  'm','n','o',
+					  'p','q','r',
+					  's','t','u',
+					  'v','w','x',
+					  'y','z'];
+
+	for(var i = 0; i< numBlanks; i++)
+	{
+		blanksAndSuccesses.push('_');
+		document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
+	}
+
+	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(' ');
+	document.getElementById('numGuesses').innerHTML = guessesLeft;
+	document.getElementById('winCounter').innerHTML = winCount;
+	document.getElementById('lossCounter').innerHTML = loseCount;
+	document.getElementById('wrongGuesses').innerHTML = wrongLetters;
+}
+
